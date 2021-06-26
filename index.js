35ad7a720c5d9a8b550c48b2f4c6dd7c34d93344
Fernando Ochoa <Fernando268a@gmail.com>
2021-06-26 15:10:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const writeOnScreen = (element)=>{
+    let elementClicked = element.target.innerText
+    screen.value += elementClicked;
+    return elementClicked
+}
+const deleteNumber = ()=> {
+    let length = screen.value.length;
+    let newValue = screen.value.slice(0,length - 1);
+    screen.value = newValue;
+}
+const doOperation = (x ,y ,operator)=>{
+    let answer;
+    if(operator === "+"){
+        return answer = x + y;
+    }
+    if(operator === "-"){
+        return answer = x - y;
+    }
+    if(operator === "x"){
+        return answer = x * y;
+    }
+    if(operator === "/"){
+        return answer = x / y;
+    }
+}
+const chooseOperation = (element)=> {
+    for (let index = 0; index < element.length; index++) {
+        const elements = element[index];
+        if(elements === "+"){
+            return "+"
+        }
+        if(elements === "-"){
+            return "-"
+        }
+        if(elements === "x"){
+            return "x"
+        }
+        if(elements === "/"){
+            return "/"
+        }
+    }
+    
+}
+const stringToNumber = (string, array) => {
+    for (let index = 0; index < string.length; index++) {
+        let element = string[index];
+        array.push(Number(element))  
+    }
+}
+const equalTo = ()=>{
+    let stringOperation = screen.value;
+    let resultOfOperator = chooseOperation(stringOperation);
+    stringOperation = stringOperation.split(resultOfOperator);
+    let arrayOfNumbers = []
+    stringToNumber(stringOperation,arrayOfNumbers)
+    let answer = doOperation(arrayOfNumbers[0],arrayOfNumbers[1],resultOfOperator);
+    screen.value = answer;
+}
+
+const screen = document.querySelector(".screen");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".arithmetic");
+const del = document.querySelector(".del");
+const equal = document.querySelector(".equal");
+
+
+numbers.forEach((element)=>{
+    element.addEventListener("click", writeOnScreen);
+})
+
+operators.forEach((element)=>{
+    element.addEventListener("click",  writeOnScreen);
+})
+
+del.addEventListener("click", deleteNumber);
+
+equal.addEventListener("click", equalTo);
